138e9386f05709137f10ece8873141a590129e36
unknown <buzzer79@gmail.com>
2019-06-13 23:41:01 +0200

    inc table

--- app.js
+++ app.js
@@ -6,20 +6,46 @@ const db = require("./database");
 
 app.use(express.urlencoded({extended:false}));
 
-const dbData = async()=>{
-    const data = await db.execute('SELECT * FROM world.city LIMIT 5');
-
-    return await data;
-}
-
-
+let cnt = 0;
 app.set("view engine","ejs");
 app.use(express.static("public"));
 
+
+
+
 app.get("/",async(req,res)=>{
+
+    const dbData = async()=>{
+        const data = await db.execute('SELECT * FROM sakila.film LIMIT 10');
+        return await data;
+    }
+    
     const data = await dbData(); 
-    console.log(data[0]);
+   
    res.render("home",{data:data[0]});
+});
+
+app.get("/next",async(req,res)=>{
+    // const add = req.params.add;
+    // console.log(add);
+    cnt +=10;
+    console.log(cnt);
+    let sqlQuery = `SELECT * FROM sakila.film LIMIT ${cnt} OFFSET 10`
+    
+
+    const dbData = async()=>{
+        
+        const data = await db.execute(sqlQuery);
+        return await data;
+        
+    }
+    
+    const data = await dbData(); 
+    console.log(data[0][1]);
+  
+
+    res.render("home",{data:data[0]});
 })
+
 
 app.listen(3000);
